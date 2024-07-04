0bfc0c7dd010aea344df9db8db446c579be49c7f
HMC8851635 <hmc8851635@HMC8851635s-MacBook-Pro.local>
2024-07-05 08:46:50 +0900

    [feat] Add car info components

---
+++ src/components/CarContentCard.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import {Box, Button, Card, CardActions, CardContent, CardMedia, Divider, Typography} from '@mui/material';
+import {useNavigate} from 'react-router-dom';
+
+interface CarContentCardParams {
+  id: number;
+  name: string;
+  brand: string;
+  year: number;
+  imageSrc: string;
+  toCompare: boolean;
+}
+
+const CarContentCard: React.FC<CarContentCardParams> = ({id, name, brand, year, imageSrc, toCompare}) => {
+  const navigate = useNavigate();
+  const onCardClick = () => {
+    console.log(id);
+    navigate(`/details/${id}`);
+  };
+
+  return (
+    <Card
+    // sx={{maxWidth: 345}}
+    >
+      <CardMedia
+        component={'img'}
+        height={'194'}
+        image={imageSrc}
+      />
+      <CardContent
+        sx={{p: 2}}
+        onClick={onCardClick}
+      >
+        <Box sx={{gap: 1}}>
+          <Typography variant={'h5'}>{name}</Typography>
+          <Box
+            style={{display: 'flex', flexDirection: 'row'}}
+            sx={{gap: 2}}
+          >
+            <Typography variant={'body1'}>{brand}</Typography>
+            <Divider
+              orientation={'vertical'}
+              flexItem
+            />
+            <Typography variant={'body1'}>{year}</Typography>
+          </Box>
+        </Box>
+      </CardContent>
+      <CardActions sx={{p: 2}}>
+        <Button
+          sx={{width: 1}}
+          variant={toCompare ? 'contained' : 'outlined'}
+        >
+          {toCompare ? '추가함' : '비교하기'}
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default CarContentCard;
---
+++ src/components/CarContentList.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import {Box, Grid} from '@mui/material';
+import CarContentCard from './CarContentCard.tsx';
+
+interface CarContentListParams {
+  items: {carId: number; image: string; year: number; brandName: string; modelName: string}[];
+}
+
+const CarContentList: React.FC<CarContentListParams> = ({items}) => {
+  return (
+    <Box sx={{flexGrow: 1}}>
+      <Grid
+        container
+        spacing={{xs: 2, md: 3}}
+        columns={{xs: 4, sm: 8, md: 12}}
+      >
+        {items.map((item) => (
+          <Grid
+            item
+            xs={2}
+            sm={4}
+            md={4}
+            key={item.carId}
+          >
+            <CarContentCard
+              id={item.carId}
+              name={item.modelName}
+              brand={item.brandName}
+              year={item.year}
+              imageSrc={item.image}
+              toCompare={false}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
+};
+
+export default CarContentList;
