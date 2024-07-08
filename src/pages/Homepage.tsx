d59b63d356022bb9683fdae1f772618f4ce0e574
르엉 도 응억 밍/웹프론트엔드개발Cell/HMC <8851635@hyundai.com>
2024-07-08 10:51:55 +0900

    [feat] add CompareBottomSheet to the Homepage

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -2,6 +2,8 @@ import {Stack, Typography} from '@mui/material';
 import CarContentList from '../components/CarContentList.tsx';
 import Filter from '../components/Filter.tsx';
 import {useState} from 'react';
+import {CompareContextProvider} from '../context/CompareContext.tsx';
+import CompareBottomSheet from '../components/CompareBottomSheet.tsx';
 
 const dummyCarList = [
   {
@@ -25,22 +27,46 @@ const dummyCarList = [
     brandName: 'Hyundai',
     modelName: 'IONIQ 6',
   },
+  {
+    carId: 4,
+    image: 'src/assets/react.svg',
+    year: 2024,
+    brandName: 'Hyundai',
+    modelName: 'The New Santa Fe',
+  },
+  {
+    carId: 5,
+    image: 'src/assets/react.svg',
+    year: 2024,
+    brandName: 'Hyundai',
+    modelName: 'IONIQ 6',
+  },
+  {
+    carId: 6,
+    image: 'src/assets/react.svg',
+    year: 2024,
+    brandName: 'Hyundai',
+    modelName: 'IONIQ 6',
+  },
 ];
 
 export default function Homepage() {
   const [searchCategory, setSearchCategory] = useState('경형');
   const [carList, setCarList] = useState([]);
 
+  // loading carlist here
+
   return (
-    <>
+    <CompareContextProvider>
       <Stack
-        spacing={2}
-        sx={{m: 2, p: 2}}
+        spacing={4}
+        sx={{ml: 2, mr: 2, p: 2, marginTop: 4, marginBottom: 20}}
       >
         <Typography variant={'h4'}>오늘 어떤 모델을 찾고 계시나요?</Typography>
         <Filter setSearchCategory={setSearchCategory} />
         <CarContentList items={dummyCarList} />
+        <CompareBottomSheet />
       </Stack>
-    </>
+    </CompareContextProvider>
   );
 }
