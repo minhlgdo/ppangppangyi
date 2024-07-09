c0f22ce364e81c1a000c99e370bda52ac01ad781
르엉 도 응억 밍/웹프론트엔드개발Cell/HMC <8851635@hyundai.com>
2024-07-09 09:14:27 +0900

    [refactor] move page implementation to AdminPage.tsx

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,9 +4,9 @@ import ErrorPage from './pages/user/ErrorPage.tsx';
 import Homepage from './pages/user/Homepage.tsx';
 import DetailPage from './pages/user/DetailPage.tsx';
 import AdminRoot from './pages/admin/AdminRoot.tsx';
-import {Box, Typography} from '@mui/material';
 import BrandPage from './pages/admin/BrandPage.tsx';
 import React from 'react';
+import AdminPage from './pages/admin/AdminPage.tsx';
 
 const router = createBrowserRouter([
   {
@@ -30,11 +30,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: '/admin',
-        element: (
-          <Box sx={{m: 4, width: '100%', height: '100%'}}>
-            <Typography>Select one item from the navigation bar to start!</Typography>
-          </Box>
-        ),
+        element: <AdminPage />,
       },
       {
         path: '/admin/brand',
