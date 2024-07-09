ba01593f5dd754a31a45b5c1606abbf266e5c150
르엉 도 응억 밍/웹프론트엔드개발Cell/HMC <8851635@hyundai.com>
2024-07-10 08:08:32 +0900

    [feat] implement CreateFuelPage.tsx

---
+++ src/common/navigation.ts
@@ -0,0 +1,3 @@
+// ADMIN FUEL PAGE
+export const FUEL_CREATE_NAV = `/admin/fuel/create`;
+export const FUEL_MAIN_NAV = `/admin/fuel`;
--- src/pages/admin/CreateFuelPage.tsx
+++ src/pages/admin/CreateFuelPage.tsx
@@ -1,3 +1,61 @@
+import {Box} from '@mui/material';
+import {PageHeader} from '@src/components/admin/PageHeader.tsx';
+import {AdminPageTypes, CREATE_RESULT_ITEMS, ResponseTypes, ResponseTypeValue} from '@src/common/constants.ts';
+import {useState} from 'react';
+import SaveComponent from '@src/components/admin/SaveComponent.tsx';
+import InputFields, {FieldType} from '@src/components/admin/InputFields.tsx';
+import {FUEL_MAIN_NAV} from '@src/common/navigation.ts';
+import {useNavigate} from 'react-router-dom';
+import ResponseDialog from '@src/components/admin/ResponseDialog.tsx';
+
 export default function CreateFuelPage() {
-  return <></>;
+  const [openDialog, setOpenDialog] = useState(false);
+  const [result, setResult] = useState(ResponseTypes.Unknown);
+  const navigate = useNavigate();
+
+  const REQUIRED_FIELDS = [
+    {
+      name: 'Fuel',
+      required: true,
+      type: FieldType.Text,
+    },
+  ];
+
+  const onSaveClick = () => {
+    // TODO: Send the received data to the server
+    // TODO: Depending on the receiving result, save the corresponding result
+    setResult(ResponseTypes.Success);
+    setOpenDialog(true);
+  };
+
+  const onCancelClick = () => {
+    navigate(FUEL_MAIN_NAV); // go back to the main page
+  };
+
+  const handleDialogClose = () => {
+    setOpenDialog(false);
+    if (result === ResponseTypes.Success) {
+      navigate(FUEL_MAIN_NAV);
+    }
+  };
+
+  const getMessage = (key: ResponseTypeValue): string => {
+    return CREATE_RESULT_ITEMS[key];
+  };
+
+  return (
+    <Box sx={{m: 4, width: '100%', height: '100%', display: 'flex', flexDirection: 'column', gap: 4}}>
+      <PageHeader pageType={AdminPageTypes.Create} />
+      <InputFields fields={REQUIRED_FIELDS} />
+      <SaveComponent
+        onSave={onSaveClick}
+        onCancel={onCancelClick}
+      />
+      <ResponseDialog
+        isOpened={openDialog}
+        text={getMessage(result)}
+        handleClose={handleDialogClose}
+      />
+    </Box>
+  );
 }
