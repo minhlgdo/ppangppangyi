38599cb6ecd1003a6522f0d3c3dcd517f3b7fa80
르엉 도 응억 밍/웹프론트엔드개발Cell/HMC <8851635@hyundai.com>
2024-07-09 16:55:28 +0900

    [feat] create common.ts to store commonly used values

---
+++ src/common/common.ts
@@ -0,0 +1,20 @@
+// CONSTANT
+export const RESPONSES = ['SUCCESS', 'FAILURE', 'UNKNOWN'];
+export type ResponseType = (typeof RESPONSES)[number];
+
+// export const EDIT_RESULT_ITEMS = ['수정이 완료되었습니다.', '수정이 실패되었습니다. 다시 시도하십시오.'];
+// export type EditResultItemType = (typeof EDIT_RESULT_ITEMS)[number];
+
+export const EDIT_RESULT_ITEMS = {
+  SUCCESS: '수정이 완료되었습니다.',
+  FAILURE: '수정이 실패되었습니다. 다시 시도하십시오.',
+  UNKNOWN: '오류가 발생되었습니다. 다시 시도하십시오.',
+};
+export type EditResultKey = keyof typeof CREATE_RESULT_ITEMS;
+
+export const CREATE_RESULT_ITEMS = {
+  SUCCESS: '등록이 완료되었습니다.',
+  FAILURE: '등록이 실패되었습니다. 다시 시도하십시오.',
+  UNKNOWN: '오류가 발생되었습니다. 다시 시도하십시오',
+};
+export type CreateResultKey = keyof typeof CREATE_RESULT_ITEMS;
