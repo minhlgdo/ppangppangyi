1d53210685d51f5fc96610b963ec7abc139874cc
르엉 도 응억 밍/웹프론트엔드개발Cell/HMC <8851635@hyundai.com>
2024-07-10 17:33:40 +0900

    [feat] create a generic item list

---
+++ src/components/admin/ListItems.tsx
@@ -0,0 +1,60 @@
+import {useNavigate} from 'react-router-dom';
+import {Divider, IconButton, List, ListItem, ListItemSecondaryAction, ListItemText} from '@mui/material';
+import React from 'react';
+import {DeleteRounded, Edit} from '@mui/icons-material';
+import {useAdminContext} from '@src/context/AdminContext.tsx';
+
+interface ListProps<T> {
+  items: T[];
+  itemKey: keyof T;
+  itemPrimaryText: keyof T;
+  itemSecondaryText?: keyof T;
+  baseItemUrl: string;
+}
+
+export default function ListItems<T>({items, itemKey, itemPrimaryText, itemSecondaryText, baseItemUrl}: ListProps<T>) {
+  const navigate = useNavigate();
+  const {setItemToDelete, setDeletePopup} = useAdminContext();
+
+  const handleEditClick = (id: T[keyof T]) => {
+    navigate(`${baseItemUrl}/${id}`);
+  };
+
+  const handleDeleteClick = (id: T[keyof T]) => {
+    setItemToDelete(id as number);
+    setDeletePopup(true);
+  };
+
+  return (
+    <List>
+      {items.map((item, index) => (
+        <React.Fragment key={item[itemKey] as number}>
+          <ListItem>
+            <ListItemText
+              primary={itemSecondaryText ? `${item[itemPrimaryText]} ${item[itemSecondaryText]}` : `${item[itemPrimaryText]}`}
+              secondary={item[itemKey] as string}
+            />
+            <ListItemSecondaryAction>
+              {/*    Edit*/}
+              <IconButton
+                style={{marginRight: 6}}
+                aria-label={'edit'}
+                onClick={() => handleEditClick(item[itemKey])}
+              >
+                <Edit />
+              </IconButton>
+              {/*Delete*/}
+              <IconButton
+                aria-label={'delete'}
+                onClick={() => handleDeleteClick(item[itemKey])}
+              >
+                <DeleteRounded />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+          {index < items.length - 1 && <Divider />}
+        </React.Fragment>
+      ))}
+    </List>
+  );
+}
