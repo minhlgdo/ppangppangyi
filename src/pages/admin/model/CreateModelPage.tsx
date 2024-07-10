57354387730937c84825742df6a7e06732130c50
르엉 도 응억 밍/웹프론트엔드개발Cell/HMC <8851635@hyundai.com>
2024-07-11 00:48:25 +0900

    [feat] implement CreateModelPage.tsx

--- src/pages/admin/model/CreateModelPage.tsx
+++ src/pages/admin/model/CreateModelPage.tsx
@@ -1,3 +1,29 @@
+import {RequiredFieldType} from '@src/common/types.ts';
+import {AdminPageTypes, FieldTypes} from '@src/common/constants.ts';
+import CreateEditLayout from '@src/layout/admin/CreateEditLayout.tsx';
+
 export default function CreateModelPage() {
-  return <></>;
+  const REQUIRED_INPUTS: RequiredFieldType[] = [
+    {
+      name: 'Model name',
+      required: true,
+      type: FieldTypes.Text,
+    },
+    {
+      name: 'Brand name',
+      required: true,
+      type: FieldTypes.Text,
+    },
+  ];
+
+  // TODO: Handle sending data to the API
+  const handleSendData = () => {};
+
+  return (
+    <CreateEditLayout
+      requiredFields={REQUIRED_INPUTS}
+      view={AdminPageTypes.Create}
+      handleSendData={handleSendData}
+    />
+  );
 }
