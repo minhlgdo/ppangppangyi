1807f7ec4f6d1c0c437f94c2d59337da6997d219
르엉 도 응억 밍/웹프론트엔드개발Cell/HMC <8851635@hyundai.com>
2024-07-11 00:57:50 +0900

    [feat] implement EditModelPage.tsx

--- src/pages/admin/model/EditModelPage.tsx
+++ src/pages/admin/model/EditModelPage.tsx
@@ -1,3 +1,37 @@
+import {RequiredFieldType} from '@src/common/types.ts';
+import {AdminPageTypes, FieldTypes} from '@src/common/constants.ts';
+import CreateEditLayout from '@src/layout/admin/CreateEditLayout.tsx';
+import {useParams} from 'react-router-dom';
+import {useState} from 'react';
+
 export default function EditModelPage() {
-  return <></>;
+  const {modelId} = useParams();
+  const [modelName, setModelName] = useState('');
+  const [brandName, setBrandName] = useState('');
+
+  const REQUIRED_INPUTS: RequiredFieldType[] = [
+    {
+      name: 'Model name',
+      required: true,
+      type: FieldTypes.Text,
+      defaultValue: modelName,
+    },
+    {
+      name: 'Brand name',
+      required: true,
+      type: FieldTypes.Text,
+      defaultValue: brandName,
+    },
+  ];
+
+  // TODO: Handle sending data to the API
+  const handleSendData = () => {};
+
+  return (
+    <CreateEditLayout
+      requiredFields={REQUIRED_INPUTS}
+      view={AdminPageTypes.Edit}
+      handleSendData={handleSendData}
+    />
+  );
 }
