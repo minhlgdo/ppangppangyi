9578d15bfd08abb14c5be2edd10e5e83d905f28a
르엉 도 응억 밍/웹프론트엔드개발Cell/HMC <8851635@hyundai.com>
2024-07-10 11:38:52 +0900

    [style] minor fix in constants.ts

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,9 +1,9 @@
 // CONSTANT
-export enum ResponseTypes {
-  Success = 'SUCCESS',
-  Failure = 'FAILURE',
-  Unknown = 'UNKNOWN',
-}
+export const ResponseTypes = {
+  Success: 'SUCCESS',
+  Failure: 'FAILURE',
+  Unknown: 'UNKNOWN',
+} as const;
 
 type ResponseTypeKey = keyof typeof ResponseTypes;
 export type ResponseTypeValue = (typeof ResponseTypes)[ResponseTypeKey];
@@ -18,7 +18,7 @@ export const CREATE_RESULT_ITEMS = {
   SUCCESS: '등록이 완료되었습니다.',
   FAILURE: '등록이 실패되었습니다. 다시 시도하십시오.',
   UNKNOWN: '오류가 발생되었습니다. 다시 시도하십시오',
-};
+} as const;
 
 export const ITEMS_PER_PAGE = 10;
 
