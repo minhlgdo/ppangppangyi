8d2e880c7411e60173cb21517ffe45aee42319cd
르엉 도 응억 밍/웹프론트엔드개발Cell/HMC <8851635@hyundai.com>
2024-07-11 16:55:09 +0900

    [feat] implement CreateCategoryPage

--- src/pages/admin/category/CreateCategoryPage.tsx
+++ src/pages/admin/category/CreateCategoryPage.tsx
@@ -1,3 +1,34 @@
+import {RequiredFieldType} from '@src/common/types.ts';
+import {AdminPageTypes, FieldTypes, PARENT_CATEGORIES, Subjects} from '@src/common/constants.ts';
+import CreateEditLayout from '@src/layout/admin/CreateEditLayout.tsx';
+
 export default function CreateCategoryPage() {
-  return <></>;
+  const REQUIRED_FIELDS: RequiredFieldType[] = [
+    {
+      name: 'parentCategory',
+      required: true,
+      type: FieldTypes.Dropdown,
+      selections: PARENT_CATEGORIES,
+    },
+    {
+      name: 'categoryName',
+      required: true,
+      type: FieldTypes.Text,
+    },
+  ];
+
+  // TODO: Handle sending data
+  const handleSendData = (data: {[key: string]: string}) => {
+    // Test input data
+    console.log(data);
+  };
+
+  return (
+    <CreateEditLayout
+      subject={Subjects.Category}
+      requiredFields={REQUIRED_FIELDS}
+      view={AdminPageTypes.Create}
+      handleSendData={handleSendData}
+    />
+  );
 }
