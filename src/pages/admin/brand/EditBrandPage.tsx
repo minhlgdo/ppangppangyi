07ed35b794c9c5a25b6f316e00b544216e99d781
르엉 도 응억 밍/웹프론트엔드개발Cell/HMC <8851635@hyundai.com>
2024-07-17 10:33:53 +0900

    [feat] connect API to EditBrandPage

--- src/pages/admin/brand/EditBrandPage.tsx
+++ src/pages/admin/brand/EditBrandPage.tsx
@@ -1,13 +1,33 @@
 import {useParams} from 'react-router-dom';
-import {useState} from 'react';
-import {AdminPageTypes, FieldTypes, Subjects} from '@src/common/constants.ts';
-import {InputValuesType, RequiredFieldType} from '@src/common/types.ts';
+import {AdminPageTypes, FieldTypes, ResponseTypes, Subjects} from '@src/common/constants.ts';
+import {Brand, InputValuesType, RequiredFieldType} from '@src/common/types.ts';
 import CreateEditLayout from '@src/layout/admin/CreateEditLayout.tsx';
-import AdminCreateEditProvider from '@src/context/AdminCreateEditContext.tsx';
+import AdminCreateEditProvider, {useDialogOpen, useResponseType} from '@src/context/AdminCreateEditContext.tsx';
+import {useMutation, useQueryClient, useSuspenseQuery} from '@tanstack/react-query';
+import {getBrand, updateBrand} from '@src/api/admin-api.ts';
+import {useEffect} from 'react';
 
-export default function EditBrandPage() {
+function EditBrandPageContent() {
   const {brandId} = useParams();
-  const [brandName, setBrandName] = useState('');
+  const {setResponseType} = useResponseType();
+  const {setDialogOpen} = useDialogOpen();
+  const queryClient = useQueryClient();
+
+  // Load brand name
+  const {data: brand, isError: fetchError} = useSuspenseQuery({
+    queryKey: ['brand', brandId],
+    queryFn: () => getBrand(brandId!),
+  });
+
+  // To prevent updating state before loading EditBrandPageContent
+  useEffect(() => {
+    if (fetchError) {
+      setDialogOpen(true);
+      setResponseType(ResponseTypes.Unknown);
+    } else {
+      setDialogOpen(false);
+    }
+  }, [fetchError, setDialogOpen, setResponseType]);
 
   const REQUIRED_INPUTS: RequiredFieldType[] = [
     {
@@ -15,26 +35,48 @@ export default function EditBrandPage() {
       label: '브랜드',
       required: true,
       type: FieldTypes.Text,
-      defaultValue: brandName,
+      defaultValue: brand?.brandName,
     },
   ];
 
-  // TODO: load brand name
+  const mutation = useMutation({
+    mutationFn: (brand: Brand) => updateBrand(brandId!, brand),
+    onSuccess: () => {
+      setResponseType(ResponseTypes.Success);
+      queryClient.invalidateQueries({queryKey: ['brands']});
+      queryClient.invalidateQueries({queryKey: ['brand', brandId]});
+    },
+    onError: () => {
+      setResponseType(ResponseTypes.Failure);
+    },
+    onSettled: () => {
+      setDialogOpen(true);
+    },
+  });
 
   // TODO: Handle sending data to the API
   const handleSendData = (data: InputValuesType) => {
     // Test
-    console.log(data);
+    const newBrand: Brand = {
+      brandName: data.brandName as string,
+    };
+    mutation.mutate(newBrand);
   };
 
   return (
+    <CreateEditLayout
+      subject={Subjects.Brand}
+      requiredFields={REQUIRED_INPUTS}
+      view={AdminPageTypes.Edit}
+      handleSendData={handleSendData}
+    />
+  );
+}
+
+export default function EditBrandPage() {
+  return (
     <AdminCreateEditProvider>
-      <CreateEditLayout
-        subject={Subjects.Brand}
-        requiredFields={REQUIRED_INPUTS}
-        view={AdminPageTypes.Edit}
-        handleSendData={handleSendData}
-      />
+      <EditBrandPageContent />
     </AdminCreateEditProvider>
   );
 }
